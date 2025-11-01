41a1d48cf7dd368716df2aee92b79d65c7b32643
Maria Kozlova <dolnovvlad@yandex.ru>
2025-11-01 19:27:41 +0300

    Написала запрос на получение версии

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,10 +1,19 @@
+import { useEffect, useState } from 'react';
 import style from './Footer.module.scss';
+import { VersionApi } from 'api/versionApi';
 
 function Footer() {
+    const [version, setVersion] = useState<string>('');
+
+    useEffect(() => {
+        VersionApi.getVersion()
+            .then(versionData => setVersion(versionData.version))
+            .catch(error => console.error('Failed to fetch version:', error));
+    }, []);
 
 	return (
         <footer className={style.footer}>
-            <span className={style.version}>v0.0.0</span>
+            <span className={style.version}>v1.0.0</span>
         </footer>
 	)
 }
